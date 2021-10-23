03f5c710eb0618a328d61707c75620f314298abb
Saravanan <vmsaravanan@hotmail.com>
2021-10-23 14:52:49 +0530

    sangam-service first commit project setup

---
+++ src/main/kotlin/com/sara/sangam/SangamServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sara.sangam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SangamServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SangamServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sara/sangam/SangamServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sara.sangam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SangamServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
